84d5b582d6db73572c555f90e71e6e8b1ae2ee54
at4pm <lushstars.nathan@gmail.com>
2024-08-31 16:03:20 +1000

    add pre-completion check and fix safe-mode incompat

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+# 1.0.4
+- Fix completion counting with a safe mode on. ([#3](https://github.com/at4pm/CompletionCounter/issues/3))
+- Add pre-completion check ([#2](https://github.com/at4pm/CompletionCounter/issues/2))
+
 # 1.0.3
 - MacOS Support
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,26 @@
 using namespace cocos2d;
 
 class $modify(LevelInfoLayer) {
-	bool init(GJGameLevel* level, bool challenge) {		
+	bool init(GJGameLevel* level, bool challenge) {
 		LevelInfoLayer::init(level, challenge);
 
 		auto levelID = std::to_string(level->m_levelID);
-		auto numToShow = geode::Mod::get()->getSavedValue<int>(levelID + "_completions", 0);
+		auto prev = geode::Mod::get()->getSavedValue<int>(levelID + "_completions", 0);
+
+		if (level->m_normalPercent == 100 && 1 > prev) {
+			geode::Mod::get()->setSavedValue(levelID + "_completions", ++prev);
+		}
 		
 		auto parent = this->getChildByID("normal-mode-bar");
 		if (parent) {
 			auto fire = CCSprite::createWithSpriteFrameName("GJ_epicCoin_001.png");
-			fire->setPosition({ -36.0f, 5.0f });
+			fire->setPosition({ -36.0f, 5.0f }); // works on my machine
 			fire->setID("completion-fire");
 				
 			auto counter = CCLabelBMFont::create("", "bigFont.fnt");
 			counter->setPosition({ 23.5f, 29.5f }); // these will be probably break with a different tp
 			counter->setID("completion-counter");
-			counter->setString(fmt::format("{}", numToShow).c_str());
+			counter->setString(fmt::format("{}", prev).c_str());
 			fire->addChild(counter);
 
 			parent->addChild(fire);
@@ -34,6 +38,7 @@ class $modify(PlayLayer) {
 	void levelComplete() {
 		PlayLayer::levelComplete();
 		if (this->m_isPracticeMode) return; // practice mode doesnt count as a completion
+		if (this->getCurrentPercent() != 100) return; // this wont cause incompats. fs
 		int levelID = this->m_level->m_levelID;
 		int prev = geode::Mod::get()->getSavedValue<int>(fmt::format("{}_completions", levelID), 0);
 		geode::Mod::get()->setSavedValue<int>(fmt::format("{}_completions", levelID), ++prev);
